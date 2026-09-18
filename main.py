109afca3efad544769f670183c63862f6c7a0dfe
seo9ky <seo9ky@gmail.com>
2026-09-18 15:30:01 +0900

    test

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("MBTI × 투자성향 분석 프로그램")
+
+
+if __name__ == "__main__":
+    main()
---
+++ supabase_clinet.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+url = os.environ["SUPABASE_URL"]
+key = os.environ["SUPABASE_KEY"]
+
+supabase: Client = create_client(url, key)
