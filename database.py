60ab2b4fe648d9f7b772957e9b09bb3a7c9c12f8
c0919ace <c0919ace@gmail.com>
2026-09-18 16:30:45 +0900

    저장/조회

--- database.py
+++ database.py
@@ -12,3 +12,57 @@ supabase = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
 )
+
+####################################
+def insert_user(name, mbti, age_group):
+    result = supabase.table("users").insert({
+        "name": name,
+        "mbti": mbti,
+        "age_group": age_group
+    }).execute()
+    return result.data[0]["user_id"]
+
+def get_all_users():
+    result = supabase.table("users").select("*").execute()
+    return result.data
+
+
+
+def insert_investment_test(user_id, risk_score, investment_type,
+                             investment_period, loss_tolerance, target_return):
+    result = supabase.table("investment_test").insert({
+        "user_id": user_id,
+        "risk_score": risk_score,
+        "investment_type": investment_type,
+        "investment_period": investment_period,
+        "loss_tolerance": loss_tolerance,
+        "target_return": target_return
+    }).execute()
+    return result.data
+
+
+
+def insert_portfolio(user_id, stock_ratio, etf_ratio, bond_ratio, cash_ratio):
+    result = supabase.table("portfolio").insert({
+        "user_id": user_id,
+        "stock_ratio": stock_ratio,
+        "etf_ratio": etf_ratio,
+        "bond_ratio": bond_ratio,
+        "cash_ratio": cash_ratio
+    }).execute()
+    return result.data
+
+
+
+def get_all_full_data():
+    users = supabase.table("users").select("*").execute().data
+    result = []
+    for u in users:
+        investment = supabase.table("investment_test").select("*").eq("user_id", u["user_id"]).execute().data
+        portfolio = supabase.table("portfolio").select("*").eq("user_id", u["user_id"]).execute().data
+        result.append({
+            **u,
+            **(investment[0] if investment else {}),
+            **(portfolio[0] if portfolio else {})
+        })
+    return result
